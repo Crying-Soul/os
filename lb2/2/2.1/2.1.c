5f10f1efbe5778f44e55964e234155999800ba3b
Crying-Soul <grebnevegor2000@mail.ru>
2025-03-07 01:50:21 +0300

    small fixes

--- lb2/2/2.1/2.1.c
+++ lb2/2/2.1/2.1.c
@@ -3,64 +3,64 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <math.h>
 
-// Имитация длительных вычислений (используется только в этом файле)
-static void long_computation(const char* process_name, int sleep_time, int* var) {
-    for (int i = 0; i < 5; i++) {
-        printf("%s: PID = %d, PPID = %d, var = %d, адрес = %p, итерация %d\n", 
-               process_name, getpid(), getppid(), *var, (void*)var, i);
-        fflush(stdout);
-        sleep(sleep_time);
+// Имитация длительных вычислений
+static void long_computation(const char* process_name, int* var) {
+    for (int i = 0; i < 100000000; i++) {
+        if (i % 10000000 == 0) {
+            printf("%-24s | PID = %-6d | PPID = %-6d | var = %-4d | адрес = %-14p | Итерация = %-10d\n", 
+                   process_name, getpid(), getppid(), *var, (void*)var, i);
+            fflush(stdout);
+        }
     }
 }
 
 int main(void) {
-    int shared_var = 100; // Локальная переменная, изменения которой в дочернем процессе не затрагивают родителя
+    int shared_var = 100; // Локальная переменная, не разделяемая между процессами
 
-    printf("Начало программы. Родитель: PID = %d, var = %d, адрес = %p\n", 
-           getpid(), shared_var, (void*)&shared_var);
+    printf("%-24s | PID = %-6d | var = %-4d | адрес = %-14p\n", 
+        "Начало программы", getpid(), shared_var, (void*)&shared_var);
     fflush(stdout);
-
+    
     pid_t pid = fork();
     if (pid < 0) {
         perror("Ошибка при вызове fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         // Дочерний процесс
-        printf("Дочерний процесс: PID = %d, PPID = %d, var = %d, адрес = %p (до изменения)\n",
-               getpid(), getppid(), shared_var, (void*)&shared_var);
+        printf("%-24s | PID = %-6d | PPID = %-6d | var = %-4d | адрес = %-14p | (до изменения)\n",
+               "Дочерний процесс", getpid(), getppid(), shared_var, (void*)&shared_var);
         fflush(stdout);
 
         shared_var += 50;  // Изменяем копию переменной в дочернем процессе
-        printf("Дочерний процесс: PID = %d, изменённое значение var = %d, адрес = %p\n", 
-               getpid(), shared_var, (void*)&shared_var);
+        printf("%-24s | PID = %-6d | PPID = %-6d | var = %-4d | адрес = %-14p | (после изменения)\n", 
+               "Дочерний процесс", getpid(), getppid(), shared_var, (void*)&shared_var);
         fflush(stdout);
 
-        long_computation("Дочерний процесс", 3, &shared_var);
+        long_computation("Дочерний процесс", &shared_var);
 
-        printf("Дочерний процесс: PID = %d, завершение работы\n", getpid());
+        printf("%-24s | PID = %-6d | Завершение работы\n", "Дочерний процесс", getpid());
         fflush(stdout);
-        _exit(EXIT_SUCCESS); // Используем _exit для корректного завершения дочернего процесса
+        _exit(EXIT_SUCCESS);
     } else {
         // Родительский процесс
-        sleep(1);  // Небольшая задержка для наглядности вывода
-
-        printf("Родитель: PID = %d, var = %d, адрес = %p (после fork)\n", 
-               getpid(), shared_var, (void*)&shared_var);
+        printf("%-24s | PID = %-6d | PPID = %-6d | var = %-4d | адрес = %-14p | (после fork)\n", 
+               "Родитель", getpid(), getppid(), shared_var, (void*)&shared_var);
         fflush(stdout);
 
-        long_computation("Родитель", 2, &shared_var);
+        long_computation("Родитель", &shared_var);
 
         if (waitpid(pid, NULL, 0) < 0) {
             perror("Ошибка при ожидании завершения дочернего процесса");
             exit(EXIT_FAILURE);
         }
 
-        printf("Родитель: PID = %d, завершение работы\n", getpid());
+        printf("%-24s | PID = %-6d | Завершение работы\n", "Родитель", getpid());
         fflush(stdout);
     }
 
-    printf("Завершение программы (PID = %d)\n", getpid());
+    printf("%-21s | PID = %-6d | Завершение программы\n", "Конец", getpid());
     fflush(stdout);
     return EXIT_SUCCESS;
 }
