922ccb488bc94a0104a4fd4bb211493334d083db
Crying-Soul <grebnevegor2000@mail.ru>
2025-03-18 15:26:55 +0300

    ps beautify

--- lb2/3/3.1/3.1-father.c
+++ lb2/3/3.1/3.1-father.c
@@ -5,48 +5,38 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+// Функция для вывода информации о процессах (SID, PID, PPID, STAT, COMM)
+void print_processes() {
+  system("ps -s | grep -v -E \"(ps|tee|sh -c)\" | tee processes_after.txt");
+}
+
+// Функция для создания процесса, отправки SIGINT и ожидания завершения
+void spawn_and_signal(const char *path, const char *proc_name) {
+  pid_t pid = fork();
+  if (pid < 0) {
+    perror("Ошибка fork");
+    exit(EXIT_FAILURE);
+  }
+  if (pid == 0) {
+    execl(path, proc_name, (char *)NULL);
+    perror("Ошибка execl");
+    exit(EXIT_FAILURE);
+  }
+  sleep(1);
+  printf("Father: Отправляем SIGINT процессу %s (PID: %d)\n", proc_name, pid);
+  kill(pid, SIGINT);
+  print_processes();
+  waitpid(pid, NULL, 0);
+}
+
 int main() {
-  pid_t son1, son2, son3;
+  // Очищаем файл перед записью
   system("> processes_after.txt");
-  // Создаем son1
-  son1 = fork();
-  if (son1 == 0) {
-    execl("./3.1-son1", "3.1-son1", NULL); // Запуск son1
-    perror("execl son1 failed");
-    exit(1);
-  }
-  sleep(1); // Даем время son1 запуститься
-  printf("Father: Sending SIGINT to son1 (PID: %d)\n", son1);
-  kill(son1, SIGINT); // Отправляем сигнал son1
-  system("ps -s | tee processes_after.txt");
-  waitpid(son1, NULL, 0); // Ждем завершения son1
 
-  // Создаем son2
-  son2 = fork();
-  if (son2 == 0) {
-    execl("./3.1-son2", "3.1-son2", NULL); // Запуск son2
-    perror("execl son2 failed");
-    exit(1);
-  }
-  sleep(1); // Даем время son2 запуститься
-  printf("Father: Sending SIGINT to son2 (PID: %d)\n", son2);
-  kill(son2, SIGINT); // Отправляем сигнал son2
-  system("ps -s | tee processes_after.txt");
-  waitpid(son2, NULL, 0); // Ждем завершения son2
+  spawn_and_signal("./lb2/3/3.1/3.1-son1", "lb2/3/3.1/3.1-son1");
+  spawn_and_signal("./lb2/3/3.1/3.1-son2", "lb2/3/3.1/3.1-son2");
+  spawn_and_signal("./lb2/3/3.1/3.1-son3", "lb2/3/3.1/3.1-son3");
 
-  // Создаем son3
-  son3 = fork();
-  if (son3 == 0) {
-    execl("./3.1-son3", "3.1-son3", NULL); // Запуск son3
-    perror("execl son3 failed");
-    exit(1);
-  }
-  sleep(1); // Даем время son3 запуститься
-  printf("Father: Sending SIGINT to son3 (PID: %d)\n", son3);
-  kill(son3, SIGINT); // Отправляем сигнал son3
-  system("ps -s | tee processes_after.txt");
-  waitpid(son3, NULL, 0); // Ждем завершения son3
-
-  printf("Father: All children have exited.\n");
+  printf("Father: Все дочерние процессы завершены.\n");
   return 0;
 }
